48ce82f009dd525a0ad13671334dbd566fbf74e5
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-07-23 16:39:47 +0100

    lesson-5 TextInputs

--- App.js
+++ App.js
@@ -1,39 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'stephen', id: '1'},
-    { name: 'opeyemi', id: '2'},
-    { name: 'sussan', id: '3'},
-    { name: 'mario', id: '4'},
-    { name: 'luigi', id: '5'},
-    { name: 'toad', id: '6'},
-    { name: 'peach', id: '7'},
-  ]);
-
-  const pressHandler = (id) => {
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id);
-    })
-  }
+  const [name, setName] = useState('Stephen');
+  const [age, setAge] = useState('23');
 
   return (
     <View style={styles.container}>
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() =>pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )} 
-      />
-      
 
-      {<StatusBar style="auto" />}
+      <Text>Enter name:</Text>
+      <TextInput 
+        placeholder='e.g. James Smith' 
+        style={styles.input}
+        onChangeText={(value) => setName(value)} />
+
+      <Text>Enter age:</Text>
+      <TextInput 
+        placeholder='e.g. 99' 
+        style={styles.input}
+        onChangeText={(value) => setAge(value)} />
+
+      <Text style={styles.result}>name: {name}, age: {age}</Text>
     </View>
   );
 }
@@ -42,18 +29,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
-    //alignItems: 'center',
-    //justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
   }
-
 });
