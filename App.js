c7629c333d0b7fe253f7caafaddda387141ac11c
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-07-27 10:41:50 +0100

    todo list app

--- App.js
+++ App.js
@@ -1,34 +1,47 @@
 import { useState } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import AddTodo from './components/addTodo';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 export default function App() {
-  const [people, setpeople] = useState([
-    { name: 'stephen',  key: '1'  },
-    { name: 'uwem',     key: '2'  },
-    { name: 'dara',     key: '3'  },
-    { name: 'clemency', key: '4'  },
-    { name: 'deen',     key: '5'  },
-    { name: 'naima',    key: '6'  },
-    { name: 'tfash',    key: '7'  },
-    { name: 'kelz',     key: '8'  },
-    { name: 'tosin',    key: '9'  }
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
   ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random().toString() },
+        ...prevTodos
+      ]
+    })    
+
+  }
 
   return (
     <View style={styles.container}>
-      
-      <ScrollView>
-        {
-          people.map((item) => {
-            return (
-              <View key={item.key}>
-                <Text style={styles.item}>{item.name}</Text>
-              </View>
-            )
-          })
-        }
-      </ScrollView>
-      
+      <Header />
+      <View style={styles.content}>
+        {/* add todo form */}
+        <AddTodo submitHandler={submitHandler} />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -37,19 +50,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20 
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'skyblue',
-    fontSize: 24,
-    textAlign:'center',
-    color: '#333',
-    borderWidth:  1,  
-    borderStyle:  'solid',
-    borderColor: 'skyblue',
-    borderRadius: 6
-  }
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
 });
