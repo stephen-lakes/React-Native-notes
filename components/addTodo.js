8f842addb47ee920147b9d6fd9b5d7a50fd965bd
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-07-27 15:49:51 +0100

    Added react import to all components

--- components/addTodo.js
+++ components/addTodo.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, TextInput, View, Button } from 'react-native';
 
 export default function AddTodo({ submitHandler }) {
--- components/todoItem.js
+++ components/todoItem.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
